11226920aea74ef79816a3abb39a6d2919fe713f
jatin989900-ux <jatin989900@gmail.com>
2026-03-05 19:32:31 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const supabase = require('../middleware/auth');
+const supabase = require('../supabase');
 const router = express.Router();
 
 const upload = multer({
